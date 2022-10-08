9877b3c0443af9e0e619121af4884041d1f85a8d
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2022-10-08 14:55:18 +0200

    controller tests

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ProductMock } from './__mocks__/product.mock';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
@@ -8,7 +11,24 @@ describe('ProductsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getRepositoryToken(Product),
+          useValue: {
+            save: jest.fn().mockResolvedValue(new ProductMock().productMock),
+            find: jest.fn().mockResolvedValue([new ProductMock().productMock]),
+            findOneBy: jest
+              .fn()
+              .mockResolvedValue(
+                new Promise(() => new ProductMock().productMock),
+              ),
+            update: jest
+              .fn()
+              .mockResolvedValue([new ProductMock().productMock]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
